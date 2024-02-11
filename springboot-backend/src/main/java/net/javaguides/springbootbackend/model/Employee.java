93153b8068fc9ed0362bee8d7a435bd0b45729e0
Daniel278J <147060394+Daniel278J@users.noreply.github.com>
2024-02-11 22:08:21 +0530

    Add files via upload

---
+++ springboot-backend/src/main/java/net/javaguides/springbootbackend/model/Employee.java
@@ -0,0 +1,67 @@
+package net.javaguides.springbootbackend.model;
+
+//import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstname;
+    
+    @Column(name = "last_name")
+    private String lastname;
+
+    @Column(name = "email")
+    private String email;
+
+    public Employee() {
+
+    }
+    
+    public Employee(String firstname, String lastname, String email) {
+        super();
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+
+}
